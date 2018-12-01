8dac6bedec7cabb66c9888ffcbf4086967849789
TheDudeFromCI <thedudefromci@gmail.com>
2018-11-30 22:50:54 -0700

    Wrote documentation for WraithEngine class.

--- WraithEngine/src/main/java/net/whg/we/main/WraithEngine.java
+++ WraithEngine/src/main/java/net/whg/we/main/WraithEngine.java
@@ -1,7 +1,20 @@
 package net.whg.we.main;
 
+/**
+ * The program entry class. This class is used for the purpose of initializing the plugin
+ * loader, all core plugins, and hunting down and launching local plugins.
+ * 
+ * @author TheDudeFromCI
+ */
 public class WraithEngine
 {
+	/**
+	 * The program entry method.
+	 * 
+	 * @param args - Arguments from command line. Used for determining program functions.
+	 * Currently, there are no extra runtime flags that can be used at this
+	 * time.
+	 */
 	public static void main(String[] args)
 	{
 		// Create plugin loader
