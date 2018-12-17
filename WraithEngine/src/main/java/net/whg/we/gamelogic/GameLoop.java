f1996efa1bd724c307287f837aa715f2e775e1e9
TheDudeFromCI <thedudefromci@gmail.com>
2018-12-17 00:40:13 -0700

    Added documentation to GameLoop interface.

--- WraithEngine/src/main/java/net/whg/we/gamelogic/GameLoop.java
+++ WraithEngine/src/main/java/net/whg/we/gamelogic/GameLoop.java
@@ -1,8 +1,19 @@
 package net.whg.we.gamelogic;
 
+/**
+ * Represents a game loop.
+ *
+ * @author TheDudeFromCI
+ */
 public interface GameLoop
 {
+	/**
+	 * Start the game loop and block until the game loop closes.
+	 */
 	public void loop();
 
+	/**
+	 * Pings the game loop to start shutting down and disposing internal resources.
+	 */
 	public void requestClose();
 }
