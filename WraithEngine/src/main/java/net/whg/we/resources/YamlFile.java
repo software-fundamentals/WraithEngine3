9bb38ce49921eef51703959ed3c3f1c340ad96e1
TheDudeFromCI <thedudefromci@gmail.com>
2019-01-16 04:48:00 -0700

    Cleaned up YamlFile class.

--- WraithEngine/src/main/java/net/whg/we/resources/YamlFile.java
+++ WraithEngine/src/main/java/net/whg/we/resources/YamlFile.java
@@ -1,6 +1,5 @@
 package net.whg.we.resources;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Map;
@@ -8,35 +7,74 @@ import java.util.Set;
 import org.yaml.snakeyaml.Yaml;
 import net.whg.we.utils.Log;
 
+/**
+ * Represents a Yaml based resource which can read and write data from
+ * ResourceFiles.
+ *
+ * @author TheDudeFromCI
+ */
 public class YamlFile
 {
 	private Map<String, Object> _map;
 
-	public boolean load(File file)
+	/**
+	 * Clears the currently held data structure, and loads a new data struction from
+	 * the requested resource file. The file will attempt to be parsed reguardless
+	 * of the file type.
+	 *
+	 * @param resource
+	 *            - The Yaml file to load.
+	 * @return True if the file was successfully loaded. False otherwise.
+	 */
+	public boolean load(ResourceFile resource)
 	{
 		Yaml yaml = new Yaml();
 		try
 		{
-			_map = yaml.load(new FileReader(file));
+			_map = yaml.load(new FileReader(resource.getFile()));
 			return true;
 		}
 		catch (FileNotFoundException e)
 		{
-			Log.errorf("Failed to load yaml file! %s", e, file.toString());
+			Log.errorf("Failed to load yaml file %s!", e, resource);
 			return false;
 		}
 	}
 
+	/**
+	 * Gets the root nodes of the data tree as a map. The map keys represent root
+	 * paths, and the values are an object representing either another tree
+	 * recursively, or a straight value for that node.
+	 *
+	 * @return A map of the root nodes of this data tree.
+	 */
 	public Map<String, Object> getRoots()
 	{
 		return _map;
 	}
 
+	/**
+	 * Gets a list of all child paths of the node at the requested path.
+	 *
+	 * @param path
+	 *            - The part of the parent node, seperated by periods.
+	 * @return A set of the names of all child nodes of the node at the requested
+	 *         path.
+	 */
 	public Set<String> getKeys(String path)
 	{
 		return getKeys(path.split("\\."));
 	}
 
+	/**
+	 * Gets a list of all child paths of the node at the requested path.
+	 *
+	 * @param path
+	 *            - The part of the parent node, where each parameter represents a
+	 *            child of the previous parameter.
+	 * @return A set of the names of all child nodes of the node at the requested
+	 *         path.
+	 */
 	@SuppressWarnings("unchecked")
 	public Set<String> getKeys(String... path)
 	{
@@ -48,19 +86,119 @@ public class YamlFile
 		return m.keySet();
 	}
 
-	public String getString(String path)
+	/**
+	 * Gets the value at the requested location, or null if the path does not exist,
+	 * or is not a value.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, seperated by periods.
+	 * @return The object value at the specified location, or null if no value is
+	 *         assigned.
+	 */
+	public Object getValue(String path)
 	{
-		return getString(path.split("\\."));
+		return getValue(path.split("\\."));
 	}
 
+	/**
+	 * Gets the value at the requested location, or null if the path does not exist,
+	 * or is not a value.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, where each parameter
+	 *            represents a child of the previous parameter.
+	 * @return The object value at the specified location, or null if no value is
+	 *         assigned.
+	 */
 	@SuppressWarnings("unchecked")
-	public String getString(String... path)
+	public Object getValue(String... path)
 	{
 		Map<String, Object> m = _map;
 
 		for (int i = 0; i < path.length - 1; i++)
 			m = (Map<String, Object>) m.get(path[i]);
 
-		return (String) m.get(path[path.length - 1]);
+		return m.get(path[path.length - 1]);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as a string.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, seperated by periods.
+	 * @return The value at the specified location as a String.
+	 * @see {@link #getValue(String)}
+	 */
+	public String getString(String path)
+	{
+		return (String) getValue(path);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as a string.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, where each parameter
+	 *            represents a child of the previous parameter.
+	 * @return The value at the specified location as a String.
+	 * @see {@link #getValue(String...)}
+	 */
+	public String getString(String... path)
+	{
+		return (String) getValue(path);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as an int.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, seperated by periods.
+	 * @return The value at the specified location as an int.
+	 * @see {@link #getValue(String)}
+	 */
+	public int getInt(String path)
+	{
+		return (int) getValue(path);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as an int.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, where each parameter
+	 *            represents a child of the previous parameter.
+	 * @return The value at the specified location as an int.
+	 * @see {@link #getValue(String...)}
+	 */
+	public int getInt(String... path)
+	{
+		return (int) getValue(path);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as a boolean.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, seperated by periods.
+	 * @return The value at the specified location as a boolean.
+	 * @see {@link #getValue(String)}
+	 */
+	public boolean getBoolean(String path)
+	{
+		return (boolean) getValue(path);
+	}
+
+	/**
+	 * Gets the value at the specified location, casted as a boolean.
+	 *
+	 * @param path
+	 *            - The path to retrieve the value from, where each parameter
+	 *            represents a child of the previous parameter.
+	 * @return The value at the specified location as a boolean.
+	 * @see {@link #getValue(String...)}
+	 */
+	public boolean getBoolean(String... path)
+	{
+		return (boolean) getValue(path);
 	}
 }
--- WraithEngine/src/test/java/resource_handling/SimpleFileDatabaseTest.java
+++ WraithEngine/src/test/java/resource_handling/SimpleFileDatabaseTest.java
@@ -3,56 +3,21 @@ package resource_handling;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import java.io.File;
 import org.junit.Test;
 import net.whg.we.main.Plugin;
 import net.whg.we.resources.ResourceFile;
 import net.whg.we.resources.SimpleFileDatabase;
+import util.CommonMock;
 
 public class SimpleFileDatabaseTest
 {
-	private SimpleFileDatabase getDatabase()
-	{
-		File workingDir = new File(System.getProperty("user.dir"));
-		return new SimpleFileDatabase(workingDir);
-	}
-
-	private Plugin getTestPlugin()
-	{
-		return new Plugin()
-		{
-
-			@Override
-			public String getPluginName()
-			{
-				return "TestPlugin";
-			}
-
-			@Override
-			public void initPlugin()
-			{
-			}
-
-			@Override
-			public void enablePlugin()
-			{
-			}
-
-			@Override
-			public int getPriority()
-			{
-				return 0;
-			}
-		};
-	}
-
 	@Test
 	public void getResource()
 	{
 		// Get a resource file and make sure it links to a file.
 
-		SimpleFileDatabase db = getDatabase();
-		Plugin plugin = getTestPlugin();
+		SimpleFileDatabase db = CommonMock.getSimpleFileDatabase();
+		Plugin plugin = CommonMock.getTestPlugin();
 
 		ResourceFile resource = db.getResourceFile(plugin, "monkey_head.fbx");
 
@@ -65,7 +30,7 @@ public class SimpleFileDatabaseTest
 	{
 		// Make sure that nothing is returned is a plugin is not defined.
 
-		SimpleFileDatabase db = getDatabase();
+		SimpleFileDatabase db = CommonMock.getSimpleFileDatabase();
 
 		ResourceFile resource = db.getResourceFile(null, "resource.obj");
 
@@ -76,7 +41,7 @@ public class SimpleFileDatabaseTest
 	{
 		// Make sure that nothing is returned if the path is broken.
 
-		SimpleFileDatabase db = getDatabase();
+		SimpleFileDatabase db = CommonMock.getSimpleFileDatabase();
 
 		ResourceFile resource = db.getResourceFile(null, "234098$%^a/asdhkjas!@#$%^&*");
 
