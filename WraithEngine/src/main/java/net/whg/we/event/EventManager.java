b952f75995ef34b240a807bffc07b5e6719e4bf5
TheDudeFromCI <thedudefromci@gmail.com>
2019-01-20 00:09:48 -0700

    Added EventManager to GameState.

--- WraithEngine/src/main/java/net/whg/we/event/EventManager.java
+++ WraithEngine/src/main/java/net/whg/we/event/EventManager.java
@@ -12,17 +12,15 @@ import net.whg.we.utils.Log;
  */
 public class EventManager
 {
-	private static ArrayList<EventCaller<?>> _eventCallers = new ArrayList<>();
+	private ArrayList<EventCaller<?>> _eventCallers = new ArrayList<>();
 
 	/**
-	 * Registers a new event caller to the database, allowing it to be referenced by
-	 * other plugins. If the event caller is already in the database, nothing
-	 * happens.
+	 * Registers a new event caller to the database, allowing it to be referenced by other plugins.
+	 * If the event caller is already in the database, nothing happens.
 	 *
-	 * @param eventCaller
-	 *            - The event called to register.
+	 * @param eventCaller - The event called to register.
 	 */
-	public static void registerEventCaller(EventCaller<?> eventCaller)
+	public void registerEventCaller(EventCaller<?> eventCaller)
 	{
 		Log.infof("Registering event caller %s", eventCaller.getName());
 		if (_eventCallers.contains(eventCaller))
@@ -45,13 +43,12 @@ public class EventManager
 	}
 
 	/**
-	 * Removes an event called from the database, and disposes it. If the event
-	 * called is not currently in the database, it is still disposed.
+	 * Removes an event called from the database, and disposes it. If the event called is not
+	 * currently in the database, it is still disposed.
 	 *
-	 * @param eventCaller
-	 *            - The event caller to unregister.
+	 * @param eventCaller - The event caller to unregister.
 	 */
-	public static void unregisterEventCaller(EventCaller<?> eventCaller)
+	public void unregisterEventCaller(EventCaller<?> eventCaller)
 	{
 		Log.infof("Unregistering event caller %s", eventCaller.getName());
 
@@ -68,32 +65,29 @@ public class EventManager
 	}
 
 	/**
-	 * Searchs the database for a specific event caller based on the name of the
-	 * event caller and returns the first reference.
+	 * Searchs the database for a specific event caller based on the name of the event caller and
+	 * returns the first reference.
 	 *
-	 * @param name
-	 *            - The name of the event caller.
+	 * @param name - The name of the event caller.
+	 *
 	 * @return The event caller, or null if it is not found.
 	 */
-	public static EventCaller<?> getEventCaller(String name)
+	public EventCaller<?> getEventCaller(String name)
 	{
 		return getEventCaller(null, name);
 	}
 
 	/**
-	 * A shorthand method for quickly finding an event caller and adding a listener
-	 * to it. If a plugin is not defined, then all plugins are checked, and the
-	 * first event caller with the requested name is returned.
+	 * A shorthand method for quickly finding an event caller and adding a listener to it. If a
+	 * plugin is not defined, then all plugins are checked, and the first event caller with the
+	 * requested name is returned.
 	 *
-	 * @param eventCallerPlugin
-	 *            - The plugin that owns the event caller, or null to search all
-	 *            plugins.
-	 * @param eventCallerName
-	 *            - The name of the event caller.
-	 * @param listener
-	 *            - The listener to add to the event caller.
+	 * @param eventCallerPlugin - The plugin that owns the event caller, or null to search all
+	 * plugins.
+	 * @param eventCallerName - The name of the event caller.
+	 * @param listener - The listener to add to the event caller.
 	 */
-	public static <T extends Listener> void registerListener(Plugin eventCallerPlugin,
+	public <T extends Listener> void registerListener(Plugin eventCallerPlugin,
 			String eventCallerName, T listener)
 	{
 		@SuppressWarnings("unchecked")
@@ -116,19 +110,16 @@ public class EventManager
 	}
 
 	/**
-	 * A shorthand method for quickly finding an event caller and removing a
-	 * listener from it. If a plugin is not defined, then all plugins are checked,
-	 * and the first event caller with the requested name is returned.
+	 * A shorthand method for quickly finding an event caller and removing a listener from it. If a
+	 * plugin is not defined, then all plugins are checked, and the first event caller with the
+	 * requested name is returned.
 	 *
-	 * @param eventCallerPlugin
-	 *            - The plugin that owns the event caller, or null to search all
-	 *            plugins.
-	 * @param eventCallerName
-	 *            - The name of the event caller.
-	 * @param listener
-	 *            - The listener to remove from the event caller.
+	 * @param eventCallerPlugin - The plugin that owns the event caller, or null to search all
+	 * plugins.
+	 * @param eventCallerName - The name of the event caller.
+	 * @param listener - The listener to remove from the event caller.
 	 */
-	public static <T extends Listener> void unregisterListener(Plugin eventCallerPlugin,
+	public <T extends Listener> void unregisterListener(Plugin eventCallerPlugin,
 			String eventCallerName, T listener)
 	{
 		@SuppressWarnings("unchecked")
@@ -151,19 +142,17 @@ public class EventManager
 	}
 
 	/**
-	 * Searchs the database for a specific event caller based on the name of the
-	 * event caller and the plugin it is assigned to and returns the first
-	 * reference. If a plugin is not specified, then only the event caller names are
-	 * considered.
+	 * Searchs the database for a specific event caller based on the name of the event caller and
+	 * the plugin it is assigned to and returns the first reference. If a plugin is not specified,
+	 * then only the event caller names are considered.
 	 *
-	 * @param plugin
-	 *            - The plugin that owns the event caller. If null, only the names
-	 *            of the event callers are checked.
-	 * @param name
-	 *            - The name of the event caller.
+	 * @param plugin - The plugin that owns the event caller. If null, only the names of the event
+	 * callers are checked.
+	 * @param name - The name of the event caller.
+	 *
 	 * @return The event caller, or null if it is not found.
 	 */
-	public static EventCaller<?> getEventCaller(Plugin plugin, String name)
+	public EventCaller<?> getEventCaller(Plugin plugin, String name)
 	{
 		if (plugin == null)
 		{
@@ -185,11 +174,11 @@ public class EventManager
 	/**
 	 * Gets a list of all event callers with the specified name.
 	 *
-	 * @param name
-	 *            - The name of the event callers to search for.
+	 * @param name - The name of the event callers to search for.
+	 *
 	 * @return A list of event callers with the specified name.
 	 */
-	public static List<EventCaller<?>> getEventCallers(String name)
+	public List<EventCaller<?>> getEventCallers(String name)
 	{
 		Log.debugf("Searching local database for all event callers with the name %s.", name);
 
@@ -207,7 +196,7 @@ public class EventManager
 	 *
 	 * @see {@link EventManager#unregisterEventCaller(EventCaller)}
 	 */
-	public static void dispose()
+	public void dispose()
 	{
 		Log.info("Disposing all event callers.");
 
--- WraithEngine/src/main/java/net/whg/we/main/GameState.java
+++ WraithEngine/src/main/java/net/whg/we/main/GameState.java
@@ -5,26 +5,55 @@ public class GameState
 	private FileDatabase _fileDatabase;
 	private ResourceLoader _resourceLoader;
 	private PluginLoader _pluginLoader;
+	private EventManager _eventManager;
 	private GraphicsPipeline _graphics;
 
 	public GameState(FileDatabase fileDatabase)
 	{
 		_fileDatabase = fileDatabase;
-
 		_resourceLoader = new ResourceLoader(_fileDatabase);
-		_resourceLoader.addFileLoader(new GLSLShaderLoader());
-		_resourceLoader.addFileLoader(new MeshSceneLoader());
-		_resourceLoader.addFileLoader(new TextureLoader());
-
 		_pluginLoader = new PluginLoader();
-		_pluginLoader.loadPluginsFromFile(_fileDatabase);
-		_pluginLoader.enableAllPlugins();
-
+		_eventManager = new EventManager();
 		_graphics = new GraphicsPipeline(resourceLoader);
 	}
 
 	public void run()
 	{
+		// Load file loaders
+		_resourceLoader.addFileLoader(new GLSLShaderLoader());
+		_resourceLoader.addFileLoader(new MeshSceneLoader());
+		_resourceLoader.addFileLoader(new TextureLoader());
+
+		// Load plugins
+		_pluginLoader.loadPluginsFromFile(_fileDatabase);
+		_pluginLoader.enableAllPlugins();
+
+		// Start game loop
 		_graphics.run();
+	}
+
+	public FileDatabase getFileDatabase()
+	{
+		return _fileDatabase;
+	}
+
+	public ResourceLoader getResourceLoader()
+	{
+		return _resourceLoader;
+	}
+
+	public PluginLoader getPluginLoader()
+	{
+		return _pluginLoader;
+	}
+
+	public EventManager getEventManager()
+	{
+		return _eventManager;
+	}
+
+	public GraphicsPipeline getGraphicsPipeline()
+	{
+		return _graphicsPipeline;
 	}
 }
