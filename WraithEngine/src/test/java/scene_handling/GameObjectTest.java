3dfb541dd0a2bb48a7962b0c610313ae82aeb2dc
TheDudeFromCI <thedudefromci@gmail.com>
2019-01-20 22:52:51 -0700

    Added tests for GameObject basic lifecycle.

--- WraithEngine/src/test/java/scene_handling/GameObjectTest.java
+++ WraithEngine/src/test/java/scene_handling/GameObjectTest.java
@@ -1,34 +1,37 @@
 package scene_handling;
 
-import org.junit.Test;
 import org.junit.Assert;
-import net.whg.we.scene.GameObject;
-import net.whg.we.scene.ObjectBehaviour;
+import org.junit.Test;
+import org.mockito.Mockito;
 import net.whg.we.scene.GameObjectManager;
+import net.whg.we.scene.GameObject;
+import net.whg.we.main.GameState;
 
 public class GameObjectTest
 {
 	@Test
 	public void initBehaviour()
 	{
+		GameState gameState = Mockito.mock(GameState.class);
+		GameObjectManager manager = new GameObjectManager(gameState);
+		GameObject go = manager.createNew();
 
+		Assert.assertEquals(manager, go.getManager());
 	}
 
 	@Test
 	public void disposeBehaviour()
 	{
+		GameState gameState = Mockito.mock(GameState.class);
+		GameObjectManager manager = new GameObjectManager(gameState);
+		GameObject go = manager.createNew();
 
-	}
+		go.destroy();
 
-	@Test
-	public void addedToManager()
-	{
+		Assert.assertFalse(go.isDisposed());
 
-	}
+		manager.endFrame();
 
-	@Test
-	public void managerDisposesObjects()
-	{
-
+		Assert.assertTrue(go.isDisposed());
 	}
 }
