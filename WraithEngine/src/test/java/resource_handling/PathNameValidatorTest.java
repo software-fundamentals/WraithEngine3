a3ceedfe752056f55e2741f15cf916cc5d20b0df
TheDudeFromCI <thedudefromci@gmail.com>
2019-01-26 23:37:19 -0700

    Updated path name validator to include resource names.

--- WraithEngine/src/test/java/resource_handling/PathNameValidatorTest.java
+++ WraithEngine/src/test/java/resource_handling/PathNameValidatorTest.java
@@ -13,6 +13,7 @@ public class PathNameValidatorTest
 
 		Assert.assertTrue(val.isValidPathName("abd.bat"));
 		Assert.assertTrue(val.isValidPathName("1230.bat"));
+		Assert.assertFalse(val.isValidPathName("12.30.bat"));
 		Assert.assertTrue(val.isValidPathName("abd_123.bat"));
 		Assert.assertTrue(val.isValidPathName("abd.bat123"));
 		Assert.assertFalse(val.isValidPathName("abdbat."));
@@ -27,5 +28,22 @@ public class PathNameValidatorTest
 		Assert.assertTrue(val.isValidPathName("123 asd/a_1/abasdd.baast123"));
 		Assert.assertTrue(val.isValidPathName("123 asd/a_1/abasd"));
 		Assert.assertFalse(val.isValidPathName("a%basd"));
+	}
+
+	@Test
+	public void pathNameWithName()
+	{
+		SimplePathNameValidator val = new SimplePathNameValidator();
+
+		Assert.assertTrue(val.isValidPathName("abd.bat:234"));
+		Assert.assertTrue(val.isValidPathName("abd.bat:23 4"));
+		Assert.assertFalse(val.isValidPathName("abd:23 4"));
+		Assert.assertTrue(val.isValidPathName("abd/nas.123:23 4"));
+		Assert.assertTrue(val.isValidPathName("abd/nas.123: d a y"));
+		Assert.assertTrue(val.isValidPathName("abd/nas.123: d  % #@$%$^&#%@"));
+		Assert.assertTrue(val.isValidPathName("a 0bd/nas.123:name"));
+		Assert.assertFalse(val.isValidPathName("a 0bd/nas.123:name:"));
+		Assert.assertTrue(val.isValidPathName("a 0bd/nas.123:name/234"));
+		Assert.assertFalse(val.isValidPathName("a 0bd/nas.123:name:name2"));
 	}
 }
