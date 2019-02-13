9a101a69ef6280167618594aecda7c636190cb6f
TheDudeFromCI <thedudefromci@gmail.com>
2019-02-12 23:11:53 -0700

    OpenGL now correctly enables face culling and texture blending.

--- WraithEngine/src/main/java/net/whg/we/rendering/opengl/OpenGLGraphics.java
+++ WraithEngine/src/main/java/net/whg/we/rendering/opengl/OpenGLGraphics.java
@@ -26,6 +26,9 @@ public class OpenGLGraphics implements Graphics
 	{
 		GL.createCapabilities();
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 		checkForErrors("Init OpenGL");
 	}
