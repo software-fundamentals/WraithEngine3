a0db8e5e26f7128dc1d973626cda8e192f7cf958
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-23 16:05:07 +0100

    Start commenting QueuedWindow functions

--- WraithEngine/src/main/java/net/whg/we/window/QueuedWindow.java
+++ WraithEngine/src/main/java/net/whg/we/window/QueuedWindow.java
@@ -3,6 +3,11 @@ package net.whg.we.window;
 import java.util.LinkedList;
 import net.whg.we.rendering.Graphics;
 
+/**
+ * The QueuedWindow class is responsible for handling the synchronized
+ * communication between the main thread and the window thread by making use
+ * of thread-safe message queues.
+ */
 public class QueuedWindow
 {
 	private Window _window;
@@ -18,12 +23,29 @@ public class QueuedWindow
 	private WindowListener _listener;
 	private boolean _cursorEnabled = true;
 
+	/**
+	 * WindowBuilder takes an engine type as argument and if valid,
+	 * initializes that window. Then a QueuedWindow is initialized
+	 * for that window as well as a Thread for the QueuedWindow.
+	 * @param  engine The type of window that should be initialized.
+	 */
+
+	/**
+	 * QueuedWindow takes a Window as argument, sets it to the current
+	 * window and assigns itself as its QueuedWindow.
+	 * @param  window The Window that the QueuedWindow should communicate with.
+	 */
 	QueuedWindow(Window window)
 	{
 		_window = window;
 		_window.setQueuedWindow(this);
 	}
 
+	/**
+	 * waitForEvents looks at the variable _requestClose and while it's false
+	 * it keeps calling the updateWindow function of _window. Inside that loop
+	 * it lets one thread look at the requests and run the first request in the line.
+	 */
 	void waitForEvents()
 	{
 		while (!_requestClose)
@@ -40,11 +62,19 @@ public class QueuedWindow
 		_window.disposeWindow();
 	}
 
+	/**
+	 * setWindowListener assigns a WindowListener to the QueuedWindow.
+	 * @param  listener The WindowListener that should be assigned.
+	 */
 	public void setWindowListener(WindowListener listener)
 	{
 		_listener = listener;
 	}
 
+	/**
+	 * getWindow returns the current Window
+	 * @return The Window assigned to the _window variable.
+	 */
 	Window getWindow()
 	{
 		return _window;
@@ -66,11 +96,20 @@ public class QueuedWindow
 		});
 	}
 
+	/**
+	 * setCursorEnabledInstant assigns true or false to the
+	 *  _cursorEnabled variable.
+	 * @param cursorEnabled boolean that should be assigned to _cursorEnabled.
+	 */
 	void setCursorEnabledInstant(boolean cursorEnabled)
 	{
 		_cursorEnabled = cursorEnabled;
 	}
 
+	/**
+	 * isCursorEnabled returns the value of _cursorEnabled.
+	 * @return true if _cursorEnabled is true, false otherwise.
+	 */
 	public boolean isCursorEnabled()
 	{
 		return _cursorEnabled;
@@ -84,6 +123,11 @@ public class QueuedWindow
 		});
 	}
 
+	/**
+	 * addRequest lets one thread add a WindowRequest to the
+	 * _request LinkedList.
+	 * @param r The WindowRequest to be added.
+	 */
 	private void addRequest(WindowRequest r)
 	{
 		synchronized (_requests)
@@ -92,6 +136,11 @@ public class QueuedWindow
 		}
 	}
 
+	/**
+	 * addEvent lets one thread add a WindowEvent to the
+	 * _events LinkedList.
+	 * @param e The WindowEvent to be added.
+	 */
 	void addEvent(WindowEvent e)
 	{
 		synchronized (_events)
@@ -100,26 +149,46 @@ public class QueuedWindow
 		}
 	}
 
+	/**
+	 * getName returns the value of _name.
+	 * @return the name of the QueuedWindow.
+	 */
 	public String getName()
 	{
 		return _name;
 	}
 
+	/**
+	 * isResizable returns the value of _isResizable.
+	 * @return true if _isResizableis true, false otherwise.
+	 */
 	public boolean isResizable()
 	{
 		return _isResizable;
 	}
 
+	/**
+	 * isVSync returns the value of _vSync.
+	 * @return true if _vSync is true, false otherwise.
+	 */
 	public boolean isVSync()
 	{
 		return _vSync;
 	}
 
+	/**
+	 * getWidth returns the width of the QueuedWindow.
+	 * @return the width of the QueuedWindow.
+	 */
 	public int getWidth()
 	{
 		return _width;
 	}
 
+	/**
+	 * getHeight returns the height of the QueuedWindow
+	 * @return the height of the QueuedWindow.
+	 */
 	public int getHeight()
 	{
 		return _height;
