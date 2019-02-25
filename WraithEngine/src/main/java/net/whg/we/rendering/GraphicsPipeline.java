1d03d565634beb6b29e9774aa45155d063a31c04
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-02-25 11:51:51 +0100

    Documentation for Window functions (#5)
    * Add comments to the WindowBuilder class
    
    * Add comments to KeyState
    
    * Start commenting QueuedWindow functions
    
    * Finish commenting the QueuedWindow functions
    
    * Add comments to GraphicsPipeline
    
    * Add comments to DefaultWindowListener
    
    * Add comments to GLFWWindow
    
    * Add comments to Window
    
    * Add comments to WindowEvent
    
    * Add comments to WindowListener
    
    * Add comments to WindowRequest
    
    * Fix typos

--- WraithEngine/src/main/java/net/whg/we/rendering/GraphicsPipeline.java
+++ WraithEngine/src/main/java/net/whg/we/rendering/GraphicsPipeline.java
@@ -5,11 +5,20 @@ import net.whg.we.utils.Screen;
 import net.whg.we.window.QueuedWindow;
 import net.whg.we.window.WindowBuilder;
 
+/**
+ * The GraphicsPipeline class initializes a window with associated
+ * graphics and sets the window of the screen.
+ */
 public class GraphicsPipeline
 {
 	private Graphics _graphics;
 	private QueuedWindow _window;
 
+	/**
+	 * GraphicsEngine initializes an OPENGL_ENGINE and a GLFW Window with
+	 * width 800 and height 600 and attaches a new DefaultWindowListener to
+	 * the window.
+	 */
 	public GraphicsPipeline()
 	{
 		_graphics = GraphicsFactory.createInstance(GraphicsFactory.OPENGL_ENGINE);
@@ -20,21 +29,35 @@ public class GraphicsPipeline
 		Screen.setWindow(_window);
 	}
 
+	/**
+	 * getGraphics returns the current Graphics.
+	 * @return the current Graphics.
+	 */
 	public Graphics getGraphics()
 	{
 		return _graphics;
 	}
 
+	/**
+	 * getWindow returns the current Window.
+	 * @return the current Window.
+	 */
 	public QueuedWindow getWindow()
 	{
 		return _window;
 	}
 
+	/**
+	 * requestClose closes the window.
+	 */
 	public void requestClose()
 	{
 		_window.requestClose();
 	}
 
+	/**
+	 * dispse destroys the window.
+	 */
 	public void dispose()
 	{
 		_graphics.dispose();
--- WraithEngine/src/main/java/net/whg/we/window/WindowEvent.java
+++ WraithEngine/src/main/java/net/whg/we/window/WindowEvent.java
@@ -1,5 +1,8 @@
 package net.whg.we.window;
 
+/**
+ * WindowEvent is an interface whose purpose is to run certain events.
+ */
 public interface WindowEvent
 {
 	public void run();
